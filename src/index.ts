b7e178db51ca32fee09b6b12b7a6865517a8c758
aetherzcode <aetherzcode@gmail.com>
2024-11-25 08:29:11 +0700

    Fix Error

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,5 @@
 import express, { Request, Response } from "express";
 import path from "path";
-import fs from "fs"; 
 import { engine } from "express-handlebars";
 import Handlebars from "handlebars";
 import cors from "cors";
@@ -16,18 +15,15 @@ const hbs = engine({
   partialsDir: path.join(__dirname, "../views/partials"),
 });
 
-Handlebars.registerHelper(
-  "includes",
-  function (str: string, substring: string) {
-    return str.includes(substring);
-  },
-);
+Handlebars.registerHelper("includes", function (str: string, substring: string) {
+  return str.includes(substring);
+});
 
 app.set("json spaces", 2);
 app.set("trust proxy", 1);
 app.use(express.json());
 app.use(cors());
-app.use(morgan("dev"));
+app.use(morgan('dev'))
 
 app.engine("jmk", hbs);
 app.set("view engine", "jmk");
@@ -35,34 +31,8 @@ app.set("views", path.join(__dirname, "../views"));
 
 app.use("/f", express.static(path.join(__dirname, "../uploads")));
 app.use(express.static(path.join(__dirname, "../public")));
-app.use(express.static(path.join(__dirname, "../node_modules")));
 
 app.use(fileRouter);
-
-app.get("/list-files", async (req: Request, res: Response) => {
-  const folderPath = path.join(__dirname, "../uploads");
-
-  try {
-    const files = await fs.promises.readdir(folderPath);
-
-    res.status(200).json({
-      message: "File list fetched successfully",
-      files,
-    });
-  } catch (error) {
-    if (error instanceof Error) {
-      res.status(500).json({
-        message: "Failed to fetch file list",
-        error: error.message,
-      });
-    } else {
-      res.status(500).json({
-        message: "Failed to fetch file list",
-        error: "An unknown error occurred",
-      });
-    }
-  }
-});
 
 app.listen(port, () => {
   console.log(`Server running at http://localhost:${port}`);
